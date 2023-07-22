0801ffa84cdd6b2f36bb5148fa935915ce15ae71
carry-agi <carry_2022@outlook.com>
2023-07-22 17:36:39 +0800

    fix conflict

--- first_node/first_node.py
+++ first_node/first_node.py
@@ -1,4 +1,7 @@
-# Copyright (c) 2022，Horizon Robotics.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2022, www.guyuehome.com
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,33 +15,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import rclpy
-from geometry_msgs.msg import Twist
-import sys
- 
- 
-def turtle_circle(radius):
-    rospy.init_node('turtlesim', anonymous=True)
-    pub = rospy.Publisher('/turtle1/cmd_vel',
-                          Twist, queue_size=10)
-    rate = rospy.Rate(10)
-    vel = Twist()
-    while not rospy.is_shutdown():
-        vel.linear.x = radius
-        vel.linear.y = 0
-        vel.linear.z = 0
-        vel.angular.x = 0
-        vel.angular.y = 0
-        vel.angular.z = 1
-        rospy.loginfo("Radius = %f",
-                      radius)
-        pub.publish(vel)
-        rate.sleep()
+"""
+@作者: 古月居(www.guyuehome.com)
+@说明: 发布速度控制话题，控制机器人圆周运动
+"""
 
-def main():
-    print('Hi from first_node.')
-    turtle_circle(2.0)
+import rclpy                                     # ROS2 Python接口库
+from rclpy.node import Node                      # ROS2 节点类
+from geometry_msgs.msg import Twist              # 速度话题的消息
 
-
-if __name__ == '__main__':
-    main()
+"""
+创建一个发布者节点
+"""
+class PublisherNode(Node):
+    
+    def __init__(self, name):
+        super().__init__(name)                                    # ROS2节点父类初始化
+        self.pub   = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)  # 创建发布者对象（消息类型、话题名、队列长度）
+        self.timer = self.create_timer(0.5, self.timer_callback)  # 创建一个定时器（单位为秒的周期，定时执行的回调函数）
+        
+    def timer_callback(self):                                     # 创建定时器周期执行的回调函数
+        twist = Twist()                                           # 创建一个Twist类型的消息对象
+        twist.linear.x  = 0.2                                     # 填充消息对象中的线速度
+        twist.angular.z = 0.8                                     # 填充消息对象中的角速度
+        self.pub.publish(twist)                                   # 发布话题消息
+        self.get_logger().info('Publishing: "linear: %0.2f, angular: %0.2f"' % (twist.linear.x, twist.angular.z))  
+        
+def main(args=None):                                 # ROS2节点主入口main函数
+    rclpy.init(args=args)                            # ROS2 Python接口初始化
+    node = PublisherNode("draw_circle")              # 创建ROS2节点对象并进行初始化
+    rclpy.spin(node)                                 # 循环等待ROS2退出
+    node.destroy_node()                              # 销毁节点对象
+    rclpy.shutdown()                                 # 关闭ROS2 Python接口
