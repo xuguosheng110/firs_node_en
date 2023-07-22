e3dffe1ac0952acb0466b7b1a753c801e330ed40
nodehubs <130040792+nodehubs@users.noreply.github.com>
2023-07-22 17:05:54 +0800

    Update first_node.py

--- first_node/first_node.py
+++ first_node/first_node.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import rospy
+import rclpy
 from geometry_msgs.msg import Twist
 import sys
  
